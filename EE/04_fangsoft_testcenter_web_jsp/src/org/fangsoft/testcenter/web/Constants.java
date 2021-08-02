dc433961a7f9b40e395e0a3f1f868daf6eb9f80e
Monologuethl <2481871325@qq.com>
2021-08-02 17:30:28 +0800

    update

--- EE/04_fangsoft_testcenter_web_jsp/src/org/fangsoft/testcenter/web/Constants.java
+++ EE/04_fangsoft_testcenter_web_jsp/src/org/fangsoft/testcenter/web/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String SESSION_TEST_RESERVATION = "session_reservation";
     public static final String SESSION_TESTRESULT = "session_testresult";
     public static final String COOKIE_USERID = "cookie_userId";
+    public static final String REQUEST_LOGIN_ERROR_MSG="ERROR";
 }
