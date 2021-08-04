b0183aefb69530d663273b2bd75cc5504b70f059
Monologuethl <2481871325@qq.com>
2021-08-04 09:30:03 +0800

    update

--- EE/05_fangsoft_testcenter_web_tag/src/org/fangsoft/testcenter/web/tag/AllTestTag.java
+++ EE/05_fangsoft_testcenter_web_tag/src/org/fangsoft/testcenter/web/tag/AllTestTag.java
@@ -2,6 +2,7 @@ package org.fangsoft.testcenter.web.tag;
 
 import jakarta.servlet.jsp.JspException;
 import jakarta.servlet.jsp.JspWriter;
+import jakarta.servlet.jsp.tagext.SimpleTagSupport;
 import org.fangsoft.testcenter.model.Test;
 import org.fangsoft.testcenter.web.JSPUtil;
 
@@ -19,5 +20,38 @@ public class AllTestTag extends SimpleTagSupport {
         this.urlTestDetail = urlTestDetail;
     }
 
+    public void doTag() throws JspException, IOException {
+        try {
+
+            List<Test> testList=JSPUtil.getTestCenterFacade().findAllTest();
+            JspWriter writer=this.getJspContext().getOut();
+            writer.println(" <table width=\"99%\" border=\"0\">");
+            writer.println("              <tr>");
+            writer.println("                <td bgcolor=\"#FFFFCC\">");
+            writer.println("                  <strong>考试</strong> ");
+            writer.println("                </td>");
+            writer.println("              </tr>");
+            writer.println("              <tr>");
+            writer.println("                <td>");
+            writer.println("                  <div align=\"left\">");
+            writer.println("                    <ul>");
+            if(testList!=null){
+                for(Test t:testList){
+                    String url=this.urlTestDetail.replace("{testId}",
+                            String.valueOf(t.getId()));
+                    writer.println("                      <li>");
+                    writer.println(JSPUtil.makeLink(url, t.getName()));
+                    writer.println("                      </li>");
+                }
+            }
+            writer.println("                    </ul>");
+            writer.println("                  </div>");
+            writer.println("                </td>");
+            writer.println("              </tr>");
+            writer.println("            </table>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
