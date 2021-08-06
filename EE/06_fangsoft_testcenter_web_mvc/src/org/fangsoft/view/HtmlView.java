c2520e29217381e2c4c744408ae33aa20094f41b
Monologuethl <2481871325@qq.com>
2021-08-06 14:18:37 +0800

    update

--- EE/06_fangsoft_testcenter_web_mvc/src/org/fangsoft/view/HtmlView.java
+++ EE/06_fangsoft_testcenter_web_mvc/src/org/fangsoft/view/HtmlView.java
@@ -1,7 +1,8 @@
 package org.fangsoft.view;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletResponse;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
